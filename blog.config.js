e17cb575512a7bc4517d6b5f1452f1b23067946f
bara <abaranowski@faradaysec.com>
2024-12-20 16:51:14 -0300

    Initial commit

---
+++ blog.config.js
@@ -0,0 +1,16 @@
+const blogConfig = {
+  title: '0xbara',
+  description: 'Mi blog personal de ciberseguridad',
+  social: {
+    github: 'https://github.com/0xbara',
+    twitter: 'https://x.com/0xbara',
+    hackerone: 'https://hackerone.com/0xbara',
+    linkedin: 'https://www.linkedin.com/in/agustinbaranowski/'
+  },
+  theme: {
+    primary: '#00ff00',
+    background: '#000000'
+  }
+}
+
+export default blogConfig
