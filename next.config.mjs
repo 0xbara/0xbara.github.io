cf28cd32c67b6d27766ff21ba4bb0c36c6f90c59
bara <bara@arch>
2024-12-28 03:07:06 -0300

    basepath

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
+  basePath: isProd ? '/blog' : '',
   reactStrictMode: true,
   output: 'export',
-  basePath: process.env.NODE_ENV === 'production' ? '/blog' : '',
   images: {
     unoptimized: true
   }
